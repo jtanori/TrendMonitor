f3b5651dba08ecbb534af35a736e709f2e30778c
Jaime Tanori <jaime.tanori@gmail.com>
2015-07-30 03:33:57 -0700

    Use oAuth 1.0A

--- Trends.js
+++ Trends.js
@@ -1,36 +1,36 @@
 var _ = require('lodash');
 var OAuth = require('oauth');
 var Promise = require('promise');
-
-var OAuth2 = OAuth.OAuth2;
-var oauth2 = new OAuth2(
+var oauth = new OAuth.OAuth(
+	'https://api.twitter.com/oauth/request_token',
+	'https://api.twitter.com/oauth/access_token',
 	process.env.TWITTER_CONSUMER_KEY,
-	process.env.TWITTER_CONSUMER_SECRET, 
-	'https://api.twitter.com/', 
+	process.env.TWITTER_CONSUMER_SECRET,
+	'1.0A',
 	null,
-	'oauth2/token', 
-	null
+	'HMAC-SHA1'
 );
 
 function getTrends(region){
 	if(!region || _.isEmpty(region)){
-		throw new Error('findTrend requires a region');
+		throw new Error('Trend.get requires a region');
 	}
 
 	var promise = new Promise(function(resolve, reject){
-		oauth2.getOAuthAccessToken(
-			'',
-			{'grant_type':'client_credentials'},
-			function (e, access_token, refresh_token, results){
-				console.log(e, access_token, refresh_token, results);
+		oauth.get(
+			'https://api.twitter.com/1.1/trends/place.json?id=' + region,
+			process.env.TWITTER_USER_TOKEN,
+			process.env.TWITTER_USER_SECRET,
+			function (e, data, res){
+				console.log(e, data, res);
+
 				if(!_.isNull(e)){
 					reject(e);
 				}else{
-					oauth2.get('https://api.twitter.com/1.1/trends/place.json?' + region, access_token, function(){
-						resolve(Array.prototype.slice.call(arguments));
-					});
-				}
-			});
+					resolve(Array.prototype.slice.call(arguments));
+				}    
+			}
+		);
 	});
 
 	return promise;
