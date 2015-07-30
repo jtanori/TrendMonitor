ba46963e08d45b4d1442c3a6cddad0d60fae5fc7
Jaime Tanori <jaime.tanori@gmail.com>
2015-07-30 02:47:43 -0700

    Create oauth client on Trends

--- Trends.js
+++ Trends.js
@@ -2,20 +2,20 @@ var _ = require('lodash');
 var OAuth = require('oauth');
 var Promise = require('promise');
 
+var OAuth2 = OAuth.OAuth2;
+var oauth2 = new OAuth2(
+	process.env.TWITTER_CONSUMER_KEY,
+	process.env.TWITTER_CONSUMER_SECRET, 
+	'https://api.twitter.com/', 
+	null,
+	'oauth2/token', 
+	null
+);
+
 function getTrends(region){
 	if(!region || _.isEmpty(region)){
 		throw new Error('findTrend requires a region');
 	}
-
-	var OAuth2 = OAuth.OAuth2;
-	var oauth2 = new OAuth2(
-		process.env.TWITTER_CONSUMER_KEY,
-		process.env.TWITTER_CONSUMER_SECRET, 
-		'https://api.twitter.com/', 
-		null,
-		'oauth2/token', 
-		null
-	);
 
 	var promise = new Promise(function(resolve, reject){
 		oauth2.getOAuthAccessToken(
@@ -23,9 +23,9 @@ function getTrends(region){
 			{'grant_type':'client_credentials'},
 			function (e, access_token, refresh_token, results){
 				if(!_.isNull(e)){
-					promise.reject(e);
+					reject(e);
 				}else{
-					promise.resolve(auth2.get('https://api.twitter.com/1.1/trends/place.json?' + region));
+					resolve(auth2.get('https://api.twitter.com/1.1/trends/place.json?' + region));
 				}
 			});
 	});
