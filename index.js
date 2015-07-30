cb8a432b10999f75caf5ab51bbad7fd3e3912f7e
Jaime Tanori <jaime.tanori@gmail.com>
2015-07-30 00:08:55 -0700

    Make it better

---
+++ index.js
@@ -0,0 +1,105 @@
+'use strict';
+
+require('newrelic');
+
+var express = require('express');
+var session = require('express-session');
+var _ = require('lodash');
+var s = require("underscore.string");
+var bodyParser = require('body-parser');
+var compression = require('compression');
+var multer = require('multer');
+var ejs = require('ejs-locals');
+var MobileDetect = require('mobile-detect');
+var helmet = require('helmet');
+var CryptoJS = require('cryptojs');
+var http = require('https');
+var parseString = require('xml2js').parseString;
+var parse = require('xml2json');
+var events = require("events");
+
+//Initialize Parse
+Parse.initialize(process.env.PARSE_APP_ID, process.env.PARSE_JS_KEY, process.env.PARSE_MASTER_KEY);
+
+//===============EXPRESS================
+// Configure Express
+var app = express();
+app.set('port', (process.env.PORT || 4000));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    rolling: true,
+    saveUninitialized: true,
+    resave: false
+}));
+app.engine('ejs', ejs);
+app.set('views', __dirname + '/views'); // Specify the folder to find templates
+app.set('view engine', 'ejs'); // Set the template engine
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(multer());
+app.use(express.static(__dirname + '/public'));
+app.use(helmet());
+//Enable cors
+app.use(function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+        next();
+    })
+    .options('*', function(req, res, next) {
+        res.end();
+    });
+
+app.locals._ = _;
+
+//===============ROUTES===============
+var title = process.env.DEFAULT_PAGE_TITLE;
+var logRequest = function(req, res, next) {
+    console.log('%s %s %s', req.method, req.url, req.path);
+    next();
+};
+
+var getDeviceExtension = function(ua) {
+    var md = new MobileDetect(ua);
+
+    return md.phone() ? 'phones' : md.tablet() ? 'tablets' : '';
+};
+
+var checkEnvironment = function(req, res, next) {
+    // Since the session check is performed in all routes we can
+    // also configure the layout
+    var device = getDeviceExtension(req.headers['user-agent']);
+    switch (device) {
+        case 'phones':
+            app.locals.LAYOUT = LAYOUT = 'phones';
+            break;
+        case 'tablets':
+            app.locals.LAYOUT = LAYOUT = 'tablets';
+            break;
+        default:
+            app.locals.LAYOUT = LAYOUT = 'main';
+            break;
+    }
+
+    next();
+};
+
+//Main router
+var Monitor = express.Router();
+
+Monitor.use(logRequest);
+
+Monitor.post('/', function(req, res) {
+    //Check attrs
+    console.log('monitor');
+});
+
+//Use CashRegister router
+app.use('/monitor', Monitor);
+
+/*===============START=================*/
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'));
+});
---
+++ trend_worker.js
@@ -0,0 +1,57 @@
+var worker = require('node_helper');
+var https = require("https");
+var http = require("http");
+var events = require("events");
+var params = worker.params;
+
+var Monitor = (function () {
+    var eventEmitter = new events.EventEmitter();
+
+    return {
+        EventEmitter:eventEmitter  // The event broadcaster
+    };
+})();
+
+Monitor.EventEmitter.once("result", function (results) {
+	console.log('Results');
+    console.log(results);
+});
+
+function monitor(){
+    console.log('Monitor task initiated');
+
+    var data = JSON.stringify({});
+	var request = http.request({
+        host:"twittertrendmonitor.herokuapp.com",
+        port:443,
+        method: "POST",
+        path:"/monitor",
+        headers: {
+	    	'Content-Type': "application/json",
+            'Content-Length': data.length
+	    },
+    })
+        .on("response", function (response) {
+            var body = "";
+
+            response.on("data", function (data) {
+                body += data;
+                try {
+                    var res = JSON.parse(body);
+                    console.log('response');
+                    console.log(res);
+                    Monitor.EventEmitter.emit("result", res);
+                    Monitor.EventEmitter.removeAllListeners("result");
+                }
+                catch (ex) {
+                    console.log("Loading...");
+                    console.log(ex);
+                }
+            });
+        });
+
+    request.write(data);
+    request.end();
+};
+
+monitor();
